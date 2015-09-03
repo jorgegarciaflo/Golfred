f680725d56b032a730846f62f33d3aa6e9f057fa
Jorge García Flores <jorgegarciaflo@gmail.com>
2015-09-03 11:13:57 -0500

    first commit

---
+++ golem/jade_workspace/sandbox/beginner_tutorials/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials/build/devel/lib:/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials/build/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials/build/devel/lib/pkgconfig:/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials/build/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/jgflores/code/Golfred/golem/jade_workspace/sandbox/beginner_tutorials:/opt/ros/jade/share:/opt/ros/jade/stacks"
