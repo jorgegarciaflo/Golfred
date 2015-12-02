5c37bbfddb608664fa5fe618f133a1ead0b0f39f
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2015-12-02 08:29:35 -0600

    First versión of webservice

---
+++ golem/server/src/golfred.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+# ----------------------------------------------------------------------
+# Golfred interface
+# ----------------------------------------------------------------------
+# Ivan Vladimir Meza-Ruiz/ ivanvladimir at turing.iimas.unam.mx
+# 2015/IIMAS/UNAM
+# ----------------------------------------------------------------------
+from __future__ import print_function
+
+import os
+
+def create_new_experience(dirname,id_experience):
+    os.mkdir(os.path.join(dirname,id_experience))
+    return True
+
+def check_experience(dirname,id_experience):
+    os.path.isdir(dirname,id_experience)
+    return True
+
+def recover_experiences(dirname):
+    experiences={}
+    if os.path.isdir(dirname):
+        for id_experience in os.listdir(dirname):
+            memories=os.listdir(os.path.join(dirname,id_experience))
+            memories=[m for m in memories if m.endswith('.jpg')]
+            experiences[id_experience]=len(memories)
+    return experiences
+
+def recover_visual_memories(dirname,id_experience):
+    print(os.path.isdir(os.path.join(dirname,id_experience)))
+
+    if os.path.isdir(os.path.join(dirname,id_experience)):
+        memories=os.listdir(os.path.join(dirname,id_experience))
+        memories=[m for m in memories if m.endswith('.jpg')]
+        return memories
+    else:
+        return False
+
+
+
+
